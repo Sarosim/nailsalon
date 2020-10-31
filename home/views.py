9e59bc8b274da7de649543ad15ea39773ffaedd1
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-10-31 23:57:31 +0000

    add the special banner both mobile and large screen

--- home/views.py
+++ home/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """ Render the home page"""
-    return render(request, "home/index.html")
+
+    # Special banner settings
+    banner_context = {
+        'banner_display': True,
+        'banner_pos_left': '60vw',
+        'banner_pos_top': '15vh',
+        'banner_width': '25vw',
+        'banner_text_1': 'Want to ',
+        'banner_button': 'get',
+        'banner_text_2': 'a FREE treatment?',
+    }
+    return render(request, "home/index.html", banner_context)
